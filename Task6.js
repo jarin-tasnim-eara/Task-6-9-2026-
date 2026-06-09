a5a1b637eecdab9c28faa4e4d0f60200e5534290
Jarin Tasnim Eara <jarintasnimeara@gmail.com>
2026-06-09 16:51:18 +0600

    Update and rename T6.html to Task6.js

---
+++ Task6.js
@@ -0,0 +1,16 @@
+let users=[
+    { name: "Eara", group:1 },
+    { name: "Jarin", group:2 },
+    { name: "Tasnim", group:1 },
+    ]
+ let grouped ={};
+
+ for( let i=0; i<users.length;i++ ){
+    let j= users[i].group;
+    if(!grouped[j]){
+        grouped[j]=[];
+    }
+    grouped[j].push(users[i]);
+ }
+
+console.log(grouped);
