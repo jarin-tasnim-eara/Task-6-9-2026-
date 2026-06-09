bb92bdf51b37fe3d34f56ae8fe134d8fd96fe7a6
Jarin Tasnim Eara <jarintasnimeara@gmail.com>
2026-06-09 17:22:50 +0600

    Update Task2.js

--- Task2.js
+++ Task2.js
@@ -1,5 +1,5 @@
 let array = [10,45,30,20];
-
+function secondLargest(array){
 let l = array[0];
 let sl = array[0];
 for(let i=1; i<array.length ; i++){
@@ -10,5 +10,8 @@ l= array [i];
 
 } else if(array[i]> sl){
 sl = array[i];
+
 }}
-console.log(sl);
+return sl;
+}
+console.log(secondLargest(array));
